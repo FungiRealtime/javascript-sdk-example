446f526ed183089ea33c960fc2a22fdefd90f546
Gabriel Mendez <yo@gabrielmendezc.com>
2021-05-07 17:25:29 +0200

    Done

--- index.ts
+++ index.ts
@@ -2,15 +2,12 @@ import { Client } from "@fungi-realtime/core";
 
 interface Message {
   content: string;
-  sentAt: Date;
+  sentAt: string;
 }
 
 // DOM Elements
 let messagesEl = document.getElementById("messages");
 let newMessageForm = document.getElementById("new-message-form");
-
-// This is where we'll store the messages
-let messages: Message[] = [];
 
 // Create a new client (there should only be one client accross the entire app)
 let client = new Client({
@@ -22,7 +19,14 @@ let messagesChannel = client.subscribe("messages");
 
 // Bind on the channel to an event with the name "new-message".
 messagesChannel.bind<Message>("new-message", (newMessage) => {
-  console.log(newMessage);
+  let newMessageEl = document.createElement("li");
+  let elTextNode = document.createTextNode(
+    `${newMessage.content} - ${newMessage.sentAt}`
+  );
+
+  newMessageEl.appendChild(elTextNode);
+
+  messagesEl.append(newMessageEl);
 });
 
 newMessageForm.addEventListener("submit", (event: any) => {
@@ -31,8 +35,12 @@ newMessageForm.addEventListener("submit", (event: any) => {
   let [input] = event.target.elements;
   let messageContent = input.value;
 
-  messagesChannel.trigger("client-new-message", {
-    content: messageContent,
-    sentAt: new Date(),
+  fetch("http://localhost:3000/send-message", {
+    method: "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify({
+      content: messageContent,
+      sentAt: new Date().toISOString(),
+    }),
   });
 });
--- server.js
+++ server.js
@@ -1,9 +1,23 @@
+const cors = require("cors");
 const express = require("express");
-const app = express();
-const port = 3000;
+const { Client } = require("@fungi-realtime/node");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+let app = express();
+let port = 3000;
+
+let client = new Client({
+  httpEndpoint: "http://localhost:8080",
+  key: "app-test-key",
+  secret: "app-test-secret",
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/send-message", async (req, res) => {
+  let { content, sentAt } = req.body;
+  await client.trigger("messages", "new-message", { content, sentAt });
+  return res.json({ content, sentAt });
 });
 
 app.listen(port, () => {
